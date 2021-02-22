a4e0487560555166a4d59b5439537f9f6342a9a8
b06501069 <59386373+b06501069@users.noreply.github.com>
2021-02-22 23:03:01 +0800

    Create try_GitHub.py
    calculate the number

---
+++ try_GitHub.py
@@ -0,0 +1,14 @@
+import math
+
+x_limit = int(input('Enter the type: '))
+y_limit = int(input('Enter a number: '))
+
+def cal(t, n):
+    if x_limit == 1:
+        print(math.log(y_limit, 2))
+
+    elif x_limit == 2:
+        print(math.log(y_limit))
+    elif x_limit == 3:
+        print(y_limit**2)
+cal(x_limit,y_limit)
